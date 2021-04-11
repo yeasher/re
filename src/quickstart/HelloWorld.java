84faa423a15d673029e795330920e4364e3ab490
yeasher <yeasher@163.com>
2021-04-11 15:14:06 +0800

    first commit

---
+++ src/quickstart/HelloWorld.java
@@ -0,0 +1,8 @@
+package quickstart;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("gggf");
+    }
+}
